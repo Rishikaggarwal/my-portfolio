fe253bc484d59531e0d845482d82a50fdee131e2
Rishikaggarwal <162909316+Rishikaggarwal@users.noreply.github.com>
2024-04-21 13:28:42 +0530

    Add files via upload

---
+++ resumejs/index.js
@@ -0,0 +1,47 @@
+
+// .projects{
+//     display: flex;
+//     justify-content: center;
+//     align-items: center;
+//     flex-direction: column;
+//     transition: all;
+//     transform:rotateY(360 degrees);
+// }
+// .circle {
+//     height: 500px;
+//     width: 500px;
+//     border: 2px solid black;
+//     background-color: #312a63;
+//     border-radius: 50%;
+//     position: relative;
+//     display: flex;
+//     flex-direction:column;
+//   }
+
+  
+
+//   .point {
+//     position: absolute;
+//     width: 20px;
+//     height: 20px;
+//     background-color: #522d5b;
+//   }
+  
+//   .top-left {   top: -10px;
+//     left: -10px;
+//   }
+  
+//   .top-right {
+//     top: -10px;
+//     right: -10px;
+//   }
+  
+//   .bottom-left {
+//     bottom: -10px;
+//     left: -10px;
+//   }
+  
+//   .bottom-right {
+//     bottom: -10px;
+//     right: -10px;
+//   }
